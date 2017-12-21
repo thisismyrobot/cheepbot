a6fdfdf139df913aea58d6d5aef224647d7af7f0
Robert Wallhead <rwallhead@gmail.com>
2017-12-21 19:46:43 +1100

    Starting to break up the code into methods and to look at the dithering

--- map_building/test.py
+++ map_building/test.py
@@ -12,22 +12,11 @@ import cv2
 import statistics
 
 
-def go(existing, new, out):
-    img_new = cv2.imread(new, 0)  # Taken 1m away.
-    img_existing = cv2.imread(existing, 0)  # Initial/existing/baseline image.
+def offsets(matches, kp_existing, kp_new):
+    """Given SIFT and knnMatch data, return the offset between the images.
 
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    kp_new, des_new = sift.detectAndCompute(img_new, None)
-    kp_existing, des_existing = sift.detectAndCompute(img_existing, None)
-
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    matches = flann.knnMatch(des_new, des_existing, k=2)
-
+    Assumes some overlap.
+    """
     offsets_x = []
     offsets_y = []
     for i,(m, n) in enumerate(matches):
@@ -43,45 +32,68 @@ def go(existing, new, out):
     offset_x = int(statistics.median(offsets_x))
     offset_y = int(statistics.median(offsets_y))
 
-    # Pad out existing with transparency.
-    img_existing = cv2.copyMakeBorder(
-        img_existing,
-#        cv2.cvtColor(img_existing, cv2.COLOR_GRAY2RGBA),
-        -offset_y if offset_y < 0 else 0,
+    return offset_x, offset_y
+
+
+def add_to_map(img_map, img_new, offset_x, offset_y):
+    """Apply a new image to an existing map and return."""
+    # Pad out new image to fit map size + offsets.
+    img_result = cv2.copyMakeBorder(
+        img_new,
         offset_y if offset_y > 0 else 0,
-        -offset_x if offset_x < 0 else 0,
+        -offset_y if offset_y < 0 else 0,
         offset_x if offset_x > 0 else 0,
+        -offset_x if offset_x < 0 else 0,
         cv2.BORDER_CONSTANT,
         value=(0, 0, 0)
     )
 
-    # position
-    if offset_x < 0:
-        # New is out to left.
-        if offset_y < 0:
-            # New is out above
-            img_existing[0:offset_y, 0:-offset_x] = img_new[0:, 0:-offset_x]
-            img_existing[0:-offset_y, -offset_x:] = img_new[0:-offset_y, 0:]
+    # Overlay the map
+    img_result[-offset_y:, -offset_x:] = img_map[0:, 0:]
+
+    # Re-overlay the new, dithered
+    out_row_start, out_row_size = 0, img_new.shape[0]
+    out_col_start, out_col_size= 0, img_new.shape[1]
+    img_result[
+        out_row_start:out_row_size:2,
+        out_col_start:out_col_size:2,
+    ] = img_new[
+        out_row_start:out_row_size:2,
+        out_col_start:out_col_size:2,
+    ]
+    img_result[
+        out_row_start+1:out_row_size:2,
+        out_col_start+1:out_col_size:2,
+    ] = img_new[
+        out_row_start+1:out_row_size:2,
+        out_col_start+1:out_col_size:2,
+    ]
+
+    return img_result
 
 
-    cv2.imwrite(out, img_existing)
+def go(existing_map, new, out):
+    img_new = cv2.imread(new, 0)  # Taken 1m away.
+    img_map = cv2.imread(existing_map, 0)  # Initial/existing/baseline image.
 
+    sift = cv2.xfeatures2d.SIFT_create()
 
-    # Pad out new with transparency.
-#    img_new = cv2.copyMakeBorder(
-#        cv2.cvtColor(img_new, cv2.COLOR_GRAY2RGBA),
-#        int(offset_y) if offset_y > 0 else 0,
-#        int(-offset_y) if offset_y < 0 else 0,
-#        int(offset_x) if offset_x > 0 else 0,
-#        int(-offset_x) if offset_x < 0 else 0,
-#        cv2.BORDER_CONSTANT,
-#        value=(0, 0, 0, 0)
-#    )
+    kp_new, des_new = sift.detectAndCompute(img_new, None)
+    kp_existing, des_existing = sift.detectAndCompute(img_map, None)
 
-#    img_result = cv2.addWeighted(img_new, 0.5, img_existing, 0.5, 0)
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-#    cv2.imwrite(out, img_result)
+    matches = flann.knnMatch(des_new, des_existing, k=2)
+
+    offset_x, offset_y = offsets(matches, kp_existing, kp_new)
+
+    img_updated_map = add_to_map(img_map, img_new, offset_x, offset_y)
+
+    cv2.imwrite(out, img_updated_map)
 
 
 if __name__ == '__main__':
-    go('first.jpg', 'second.jpg', 'matches1.png')
+    go('first.jpg', 'second.jpg', 'combined.png')
