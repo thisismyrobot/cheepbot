3eba9e36da232aab593b809fe098f3e6c63501b8
Robert Wallhead <rwallhead@gmail.com>
2018-01-14 14:30:18 +1100

    Enabled coast mode

--- drive/commands/__init__.py
+++ drive/commands/__init__.py
@@ -1,7 +1,15 @@
 """Just the basic commands for now."""
 def forward(speed=0x10):
-    return bytearray([0x8d, speed, 0x89, speed])
+    return bytearray([
+        0x86, 0x87,  # coast enabled
+        0x8d, speed,  # right forward at speed
+        0x89, speed  # left forward at speed
+    ])
 
 
 def stop():
-    return bytearray([0x8d, 0x0, 0x89, 0x0])
+    return bytearray([
+        0x86, 0x87, # coast enabled
+        0x8d, 0x0,  # right stop
+        0x89, 0x0  # left stop
+    ])
