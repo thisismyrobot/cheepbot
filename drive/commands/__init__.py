e9136be1d8aad4a2de819f2722248def4f16764f
Robert Wallhead <rwallhead@gmail.com>
2018-01-14 14:31:31 +1100

    Misunderstood coast....

--- drive/commands/__init__.py
+++ drive/commands/__init__.py
@@ -1,7 +1,6 @@
 """Just the basic commands for now."""
 def forward(speed=0x10):
     return bytearray([
-        0x86, 0x87,  # coast enabled
         0x8d, speed,  # right forward at speed
         0x89, speed  # left forward at speed
     ])
@@ -9,7 +8,5 @@ def forward(speed=0x10):
 
 def stop():
     return bytearray([
-        0x86, 0x87, # coast enabled
-        0x8d, 0x0,  # right stop
-        0x89, 0x0  # left stop
+        0x86, 0x87,  # coast enabled - slows down without braking.
     ])
