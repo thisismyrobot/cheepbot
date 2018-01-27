14560ebfe17d326d3f9d093a6f282d354c57236b
Robert Wallhead <rwallhead@gmail.com>
2018-01-27 13:05:32 +1100

    Accounting for compass orientation on the bot itself

--- orientation/serve.py
+++ orientation/serve.py
@@ -14,6 +14,7 @@ def get_orientation():
         conn.write(bytearray([0x13]))  # 16-bit direction
         dir_bytes = conn.read(2)
         orientation = int.from_bytes(dir_bytes, byteorder='big') / 10
+        orientation = (orientation + 180) % 360  # The compass is 180 degrees off when installed.
         return flask.jsonify({'orientation': orientation})
 
 
